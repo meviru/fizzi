71c3c139486b3648781e145a30dd4c876075224c
Viral Patel <viralpatel508@gmail.com>
2024-10-03 18:51:41 +0530

    feat: initial page setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
 import "./app.css";
+import Header from '@/components/Header';
 
 const alpino = localFont({
   src: '../../public/fonts/Alpino-Variable.woff2',
@@ -11,7 +12,7 @@ const alpino = localFont({
   weight: "100 900",
   variable: "--font-alpino"
 })
- 
+
 
 export default function RootLayout({
   children,
@@ -20,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={alpino.variable}>
-      <body>{children}</body>
+      <body className='overflow-x-hidden bg-yellow-300'>
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
