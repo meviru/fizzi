5fb6a61286da8980e6508d4c531fb00686bdf163
Viral Patel <viralpatel508@gmail.com>
2024-10-11 11:20:33 +0530

    fix: component display name issue

---
+++ src/hooks/useStore.ts
@@ -0,0 +1,11 @@
+import { create } from 'zustand'
+
+interface State {
+    ready: boolean,
+    isReady: () => void;
+}
+
+const useStore = create<State>((set) => ({
+    ready: false,
+    isReady: () => set({ ready: true }),
+}))
