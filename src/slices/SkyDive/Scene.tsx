5aa66aa8cbb386e31f38ea3532bfeb9dfcd9096b
Viral Patel <viralpatel508@gmail.com>
2024-10-14 15:57:49 +0530

    feat: carousel section animation

--- src/slices/SkyDive/Scene.tsx
+++ src/slices/SkyDive/Scene.tsx
@@ -110,6 +110,19 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
                 duration: 0.3,
                 ease: "back.out(1.7)"
             })
+            .to(wordsRef.current.children.map(word => word.position), {
+                keyframes: [
+                    { x: 0, y: 0, z: -1 },
+                    { ...getXYPosition(-7), z: -7 }
+                ],
+                stagger: 0.3
+            }, 0)
+            .to(canRef.current.position, {
+                ...getXYPosition(4),
+                duration: 0.5,
+                ease: "back.in(1.7)"
+            })
+            .to(cloudsRef.current.position, { z: 7, duration: 0.5 })
     })
 
     return (
@@ -120,7 +133,9 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
                     rotationIntensity={0}
                     floatIntensity={3}
                     floatSpeed={3}
-                ></FloatingCan>
+                >
+                    <pointLight intensity={30} color="#8c0413" decay={0.6} />
+                </FloatingCan>
             </group>
 
             <Clouds ref={cloudsRef}>
